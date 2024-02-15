7b40ac58a03f3342f7b6389055c25e59969f4d35
adarrssh <adarsh00502@gmail.com>
2024-02-15 12:02:34 +0530

    testing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -54,8 +54,27 @@ export const Home = () => {
             navigate(`/room/${room}`);
         },
         [navigate]
-    );
+    );  
 
+    const [pageLoaded, setPageLoaded] = useState(false);
+
+    useEffect(() => {
+      const handlePageLoad = () => {
+        setPageLoaded(true);
+      };
+  
+      // Add event listener for the page load event
+      window.addEventListener('load', handlePageLoad);
+  
+      // Cleanup: Remove the event listener when the component unmounts
+      return () => {
+        window.removeEventListener('load', handlePageLoad);
+      };
+    }, []);
+
+    useEffect(()=>{
+        console.log(pageLoaded)
+    })
     useEffect(()=>{
         if(roomId){
             setRoomId("")
