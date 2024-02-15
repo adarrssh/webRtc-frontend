df73cbaa90e6162b653e99f13e0d43e38d0d8cfc
adarrssh <adarsh00502@gmail.com>
2024-02-16 00:02:49 +0530

    refactor: App.js

--- src/App.js
+++ src/App.js
@@ -9,24 +9,6 @@ function App() {
 
   const [pageLoaded, setPageLoaded] = useState(false);
 
-  useEffect(() => {
-    const handlePageLoad = () => {
-      setPageLoaded(true);
-    };
-
-    // Add event listener for the page load event
-    window.addEventListener('load', handlePageLoad);
-
-    // Cleanup: Remove the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('load', handlePageLoad);
-    };
-  }, []);
-
-  useEffect(()=>{
-      console.log('inside app :: ' + pageLoaded)
-  })
-
   return (
     <div className="App">
         <Routes>
